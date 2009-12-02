d6c06cff1165397f8c5dbf50b8a54fd7f37112c0
Pat Allan <pat@freelancing-gods.com>
2009-12-02 11:43:56 +1100

    Adding explicit delayed delta require - TS seems to exist already.

--- lib/thinking_sphinx/deltas/delayed_delta/tasks.rb
+++ lib/thinking_sphinx/deltas/delayed_delta/tasks.rb
@@ -6,6 +6,7 @@ namespace :thinking_sphinx do
   desc "Process stored delta index requests"
   task :delayed_delta => :app_env do
     require 'delayed/worker'
+    require 'thinking_sphinx/deltas/delayed_delta'
     
     Delayed::Worker.new(
       :min_priority => ENV['MIN_PRIORITY'],
