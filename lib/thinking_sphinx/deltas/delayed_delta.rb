c1198f383fc395e8c2494bd19b32ea422d171243
Maximilian Schulz <namxam@noname.(none)>
2010-02-26 21:38:29 +0100

    Explicitly require thinking_sphinx in order to get it working with bundler

--- lib/thinking_sphinx/deltas/delayed_delta.rb
+++ lib/thinking_sphinx/deltas/delayed_delta.rb
@@ -1,5 +1,5 @@
 require 'delayed_job'
-
+require 'thinking_sphinx'
 require 'thinking_sphinx/deltas/delayed_delta/delta_job'
 require 'thinking_sphinx/deltas/delayed_delta/flag_as_deleted_job'
 require 'thinking_sphinx/deltas/delayed_delta/job'
