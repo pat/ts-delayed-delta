f7979e638cea5311118b3a72fc41cef8161cca51
Pat Allan <pat@freelancing-gods.com>
2011-11-26 09:24:32 +0700

    Removing duplicate of rake tasks, cleaning up railtie.

--- lib/thinking_sphinx/deltas/delayed_delta/railtie.rb
+++ lib/thinking_sphinx/deltas/delayed_delta/railtie.rb
@@ -1,14 +1,5 @@
-module ThinkingSphinx
-  module Deltas
-
-      class DelayedDelta::Railtie < Rails::Railtie
-        engine_name :ts_delayed_delta
-
-        rake_tasks do
-          load "thinking_sphinx/deltas/delayed_delta/railties/tasks.rake"
-        end
-      end
-
+class ThinkingSphinx::Deltas::DelayedDelta::Railtie < Rails::Railtie
+  rake_tasks do
+    load 'thinking_sphinx/deltas/delayed_delta/tasks.rb'
   end
 end
-
--- lib/thinking_sphinx/deltas/delayed_delta/tasks.rb
+++ lib/thinking_sphinx/deltas/delayed_delta/tasks.rb
@@ -1,13 +1,15 @@
 namespace :thinking_sphinx do
   task :index do
+    require 'thinking_sphinx/deltas/delayed_delta'
     ThinkingSphinx::Deltas::Job.cancel_thinking_sphinx_jobs
   end
-  
+
   desc "Process stored delta index requests"
   task :delayed_delta => :app_env do
+    require 'delayed_job'
     require 'delayed/worker'
     require 'thinking_sphinx/deltas/delayed_delta'
-    
+
     Delayed::Worker.new(
       :min_priority => ENV['MIN_PRIORITY'],
       :max_priority => ENV['MAX_PRIORITY']
